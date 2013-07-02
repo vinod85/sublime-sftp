48c33ce21b5b32f6f46621147cb7f81d21cd89df
vinod (mercury) <vinod@mettlenetworks.com>
2013-07-02 15:58:45 +0530

    add,edit,delete sftp settings

--- sftp.py
+++ sftp.py
@@ -3,7 +3,64 @@
 # Fri Jun 28 08:19:57 IST 2013
 
 import sublime, sublime_plugin
+import os
 
-class sftpsetupCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		self.view.insert(edit, 0, "Hello, World!")
+class Settings:
+	@staticmethod
+	def getDefaultSettings():
+			return """{
+	"user" : "",
+	"password" : "",
+	"default_path" : ""
+}"""
+
+class sftpsetupCommand(sublime_plugin.WindowCommand):
+	def __init__(self, win):
+		self.window = win
+	def run(self):
+		new_view = self.window.new_file()
+		new_view.set_name('untitled')
+		edit = new_view.begin_edit()
+		new_view.insert(edit, 0, str(Settings.getDefaultSettings()))
+
+class sftpeditCommand(sublime_plugin.WindowCommand):
+	def __init__(self, win):
+		self.window = win
+		self.servers = []
+
+	def find_server_settings(self):
+		folder = sublime.packages_path() + '/User/sftp_servers'
+		for filename in os.listdir (folder):
+			self.servers.append(filename)
+
+	def on_select(self,picked):
+		if picked == -1:
+			return
+
+		filename = ''.join([sublime.packages_path(), '/User/sftp_servers/', self.servers[picked])
+		self.window.open_file(filename)
+
+	def run(self):
+		self.find_server_settings()
+		self.window.show_quick_panel(self.servers, self.on_select)
+
+class sftpdeleteCommand(sublime_plugin.WindowCommand):
+	def __init__(self, win):
+		self.window = win
+		self.servers = []
+
+	def find_server_settings(self):
+		folder = sublime.packages_path() + '/User/sftp_servers'
+		for filename in os.listdir (folder):
+			self.servers.append(filename)
+
+	def on_select(self,picked):
+		if picked == -1:
+			return
+
+		filename = ''.join([sublime.packages_path(), '/User/sftp_servers/', self.servers[picked])
+		os.remove(filename)
+
+    def run(self):
+		self.find_server_settings()
+		self.window.show_quick_panel(self.servers, self.on_select)
